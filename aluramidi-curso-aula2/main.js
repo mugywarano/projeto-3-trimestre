baa9bd9bad1b02a1ee00c3da6f17e4c9515c3265
mugywarano <129194529+mugywarano@users.noreply.github.com>
2023-10-24 11:28:28 -0300

    Add files via upload

---
+++ aluramidi-curso-aula2/main.js
@@ -0,0 +1,20 @@
+function tocaSomPom () {
+    document.querySelector('#som_tecla_pom').play();
+}
+
+document.querySelector('.tecla_pom').onclick = tocaSomPom;
+function tocaSomclap () {
+    document.querySelector('#som_tecla_clap').play();
+}
+
+document.querySelector('.tecla_clap').onclick = tocaSomclap;
+function tocaSomtim () {
+    document.querySelector('#som_tecla_tim').play();
+}
+
+document.querySelector('.tecla_tim').onclick = tocaSomtim;
+
+
+
+
+
