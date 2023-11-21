8e47cbcf771b37541f7974397c8aa81cb325ff91
mugywarano <129194529+mugywarano@users.noreply.github.com>
2023-11-21 11:35:37 -0300

    Add files via upload

--- aluramidi-curso-aula2/main.js
+++ aluramidi-curso-aula2/main.js
@@ -1,24 +1,21 @@
 function tocaSom(idElementoAudio){
-    document.querySelector (idElementoAudio).play();
+    document.querySelector(idElementoAudio).play();
 }
 
-const listaDeTeclas = document.querySelectorAll(".tecla");
+const ListaDeTeclas = document.querySelectorAll(".tecla");
 
-listaDeTecla [0].onclick = document.querySelectorAll("tecla");
+ListaDeTeclas[0].onclick = document.querySelectorAll(".tecla");
 
 let contador = 0;
 
-while(contador < listaDeTeclas.length){
-    const efeito = listaDeTecla[contador].classList[1]
-    const ideAudio = "#som_"+efeito;
-    listaDeTeclas[contador].onclick = function(){
-tocaSom(ideAudio);
-    }
-    contador = contador + 1;
-    
+while(contador < ListaDeTeclas.length){
+const efeito = ListaDeTeclas[contador].classList [1]
+const idAudio = "#som_"+efeito;
+ListaDeTeclas[contador].onclick = function(){
+tocaSom(idAudio);
 }
-
-
+contador = contador + 1;
+}
 
 
 
